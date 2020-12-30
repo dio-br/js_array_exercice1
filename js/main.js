eecf3bd212062273e2360d53e4014002fdb7da49
Mohammed <el-med7@outlook.com>
2020-12-30 14:33:52 +0100

    exo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+let codding = [`coding 15`,`coding 16`,`coding 17`]
+
+// 1
+
+console.log(codding)
+
+// 2
+
+console.log(codding.length);
+
+// 3 
+
+console.log(codding[0])
+console.log(codding[1])
+console.log(codding[2])
